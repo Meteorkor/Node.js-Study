5ac6fcee768f3b20fee2ae76d65ce46701720f31
unseok.kim <unseok.kim@linecorp.com>
2022-02-11 23:27:58 +0900

    first commit

---
+++ test/basic_test.js
@@ -0,0 +1,15 @@
+var assert = require('assert')
+var even_or_odd = require('../src/code_war')
+
+describe('Array', function () {
+  describe('#even_or_odd()', function () {
+    it('even_or_odd', function () {
+      even_or_odd.print();
+      assert.equal(even_or_odd(2), "Even");
+      assert.equal(even_or_odd(7), "Odd");
+      assert.equal(even_or_odd(-42), "Even");
+      assert.equal(even_or_odd(-7), "Odd");
+      assert.equal(even_or_odd(0), "Even");
+    });
+  });
+});
